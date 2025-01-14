3123dcb4e66ae4d5c124aab1c86767451d2da194
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-01-14 14:29:19 +0100

    refacto

--- src/Controller/TeachersController.php
+++ src/Controller/TeachersController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Repository\ChaptersRepository;
 use App\Repository\CoursesRepository;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
